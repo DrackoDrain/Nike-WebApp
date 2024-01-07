981e1d76bbd02eb0c98349f1d9d0c089e0bd5570
Ayush <ayush0999aryan@gmail.com>
2024-01-07 11:54:36 +0530

    finalTouch

--- src/constants/index.js
+++ src/constants/index.js
@@ -111,8 +111,8 @@ export const footerLinks = [
     {
         title: "Get in touch",
         links: [
-            { name: "customer@nike.com", link: "mailto:customer@nike.com" },
-            { name: "+92554862354", link: "tel:+92554862354" },
+            { name: "Aryan@nike.com", link: "mailto:customer@nike.com" },
+            { name: "+91-7462833534", link: "tel:+92554862354" },
         ],
     },
 ];
